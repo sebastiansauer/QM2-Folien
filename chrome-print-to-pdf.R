cf24d54db488160965070cfdd249a4e7633c4bd0
Sebastian Sauer <ssauer@posteo.de>
2021-10-29 16:58:52 +0200

    update thema4

--- chrome-print-to-pdf.R
+++ chrome-print-to-pdf.R
@@ -3,8 +3,8 @@ thema4 <- "QM2-Thema4-Gaussmodelle"
 f2 <- "Test2"
 
 
-pagedown::chrome_print(paste0("Themen/", thema3, ".Rmd"),
-                       output = paste0("Themen/", thema3, ".pdf"),
+pagedown::chrome_print(paste0("Themen/", thema4, ".Rmd"),
+                       output = paste0("Themen/", thema4, ".pdf"),
                        wait = 10,
                        timeout = 120)
 
