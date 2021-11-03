7ed16031b3bedd3e81604ff36aec302e6b918903
Sebastian Sauer <ssauer@posteo.de>
2021-11-03 18:56:12 +0100

    update thema5

--- chrome-print-to-pdf.R
+++ chrome-print-to-pdf.R
@@ -1,10 +1,15 @@
 thema3 <- "QM2-Thema3-Post-befragen"
 thema4 <- "QM2-Thema4-Gaussmodelle"
+thema5 <- "QM2-Thema5-Lineare-Modelle"
+thema6 <- "QM2-Thema6-Deskriptive-Forschungsfragen"
 f2 <- "Test2"
 
+sourcefile <- paste0("Themen/", thema5, ".Rmd")
+outputfile <- paste0("Themen/", thema5, ".pdf")
 
-pagedown::chrome_print(paste0("Themen/", thema4, ".Rmd"),
-                       output = paste0("Themen/", thema4, ".pdf"),
-                       wait = 10,
+
+pagedown::chrome_print(sourcefile,
+                       output = outputfile,
+                       wait = 30,
                        timeout = 120)
 
