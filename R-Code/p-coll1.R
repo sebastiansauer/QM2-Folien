af4fbd0f754cc66e0e4bb0881bf844eacf91195b
Sebastian Sauer <ssauer@posteo.de>
2021-12-01 16:26:56 +0100

    p-coll1.R

---
+++ R-Code/p-coll1.R
@@ -0,0 +1,28 @@
+
+myf <- function(x) -x+0.75
+
+myf2 <- function(x) -x + 1.25
+
+
+d2 <- tibble(
+  x = runif(n),
+  y = runif(n),
+  status = case_when(
+    y > myf(x) & y < myf2(x) ~ TRUE,
+    TRUE ~ FALSE
+  )
+)
+
+
+p_coll1 <-
+  d2 %>%
+  ggplot() +
+  aes(x  = x,
+      y = y) +
+  geom_point() +
+  # scale_color_manual(values = c("grey80", "black")) +
+  theme_bw() +
+  labs(x = "Looks",
+       y = "Talent") +
+  theme(legend.position = "bottom",
+        axis.text = element_blank())
